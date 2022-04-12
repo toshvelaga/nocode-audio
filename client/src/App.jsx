c29b98f9ac1d005a27167e36291cd6f1c6174299
Tosh Velaga <toshvelaga@gmail.com>
2022-04-11 20:06:01 -0600

    delete App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import CustomizePlayer from './pages/CustomizePlayer/CustomizePlayer'
 import EmbeddablePlayer from './pages/EmbeddablePlayer/EmbeddablePlayer'
 import NotFound from './pages/NotFound/NotFound'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 
 function App() {
   return (
