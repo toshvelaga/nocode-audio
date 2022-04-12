b84c19ccd117e32a52bd6d039f054156c96375d5
Tosh Velaga <toshvelaga@gmail.com>
2022-04-11 20:33:54 -0600

    add post route for audio-player

--- server/routes/audioPlayer.js
+++ server/routes/audioPlayer.js
@@ -13,4 +13,37 @@ router.get('/audio-player/:id', async (req, res) => {
   return res.json(playerData.rows[0])
 })
 
+router.post('/audio-player', async (req, res) => {
+  const {
+    title,
+    subtitle,
+    backgroundColor,
+    progressBarColor,
+    fontColor,
+    audioUrl,
+    imageUrl,
+  } = req.body
+  //   const title = 'hello'
+  //   const subtitle = 'world'
+  //   const backgroundColor = 'purple'
+
+  const newAudioPlayer = await pool.query(
+    'INSERT INTO audio_player (title, subtitle, background_color, progress_bar_color, font_color, audio_url, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+    [
+      title,
+      subtitle,
+      backgroundColor,
+      progressBarColor,
+      fontColor,
+      audioUrl,
+      imageUrl,
+    ]
+  )
+
+  const newAudioPlayerId = newAudioPlayer.rows[0].player_id
+
+  console.log(newAudioPlayer.rows[0])
+  return res.json({ id: newAudioPlayerId })
+})
+
 export default router
